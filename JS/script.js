7b9f06e8b4b14ebe18e8a2eaafa5f0f58d3f694e
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-20 14:24:29 +0300

    coming up strong

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle= document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if (navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+// removing menu on mobile
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(x => x.addEventListener('click', linkAction))
