c25f4e602a179952e7bfe93b0ccff0d58710c07a
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-21 16:25:04 +0300

    working pretty well

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,36 @@
-
 // Showing Menu
-const navMenu = document.getElementById('nav-menu'),
-      navToggle= document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-if (navToggle){
-    navToggle.addEventListener('click', () => {
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
-if (navClose){
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 // removing menu on mobile
 const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach(x => x.addEventListener('click', linkAction))
+navLink.forEach((x) => x.addEventListener("click", linkAction));
 
 // Shadow Header
 
 const shadowHeader = () => {
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add(`shadow-header`): header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add(`shadow-header`)
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
