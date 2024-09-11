e0ed15f04568b83f6d075a496db7e67a2df10a87
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2024-09-11 21:39:57 +0300

    Fully Functional and improved

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const navMenu = document.getElementById("nav_menu");
 const navToggle = document.getElementById("nav_toggle");
 const navClose = document.getElementById("nav_close");
 const header = document.querySelector("header");
+const navLinks = document.querySelector("nav_link");
 
-// show menu
 function showNavbar() {
-  if (navToggle) {
-    navToggle.addEventListener("click", () => {
-      navMenu.classList.add("show_menu");
-    });
-  }
+  navMenu.classList.add("show_menu");
 }
-// hide menu
-
 function hideNavBar() {
-  if (navClose) {
-    navClose.addEventListener("click", () => {
-      navMenu.classList.remove("show_menu");
-    });
-  }
+  navMenu.classList.remove("show_menu");
+}
+
+navLinks.foreach((link) => {
+  link.addEventListener("click", hideNavBar);
+});
+
+if (navClose) {
+  navClose.addEventListener("click", hideNavBar);
 }
 
 window.onscroll = () => {
